bec7e9044a5300d5b6784ef7a3ba92947c50a7cd
Diorgenes M. Silva <diorgenesmorais@gmail.com>
2024-11-19 20:35:43 -0300

    feat: create Logo template

--- apps/frontend/src/components/template/Page.tsx
+++ apps/frontend/src/components/template/Page.tsx
@@ -1,3 +1,4 @@
+import Logo from './Logo';
 
 export interface PageProps {
     children: React.ReactNode;
@@ -6,7 +7,8 @@ export interface PageProps {
 export default function Page(props: PageProps) {
     return (
         <div className="flex flex-col items-center py-10 min-h-screen bg-[url('/background.png')] bg-cover">
-            <main className={props.className}>
+            <Logo />
+            <main className={`flex-1 flex flex-col justify-center py-10 container ${props.className}`}>
                 {props.children}
             </main>
         </div>
