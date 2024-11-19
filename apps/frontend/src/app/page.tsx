fa574cc9873f895be8d4be9459765d3b3791cc16
Diorgenes M. Silva <diorgenesmorais@gmail.com>
2024-11-19 20:02:40 -0300

    feat: create project

---
+++ apps/frontend/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Page from "@/components/template/Page";
+
+export default function Home() {
+  return (
+    <Page>
+      <div className="">
+        Inicio
+      </div>
+    </Page>
+  );
+}
