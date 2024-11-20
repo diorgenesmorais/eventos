20a1d0979c446961563eb818336b5c3350a7c7e5
Diorgenes M. Silva <diorgenesmorais@gmail.com>
2024-11-20 17:08:03 -0300

    feat: create createEmptyEvent function

--- apps/frontend/src/core/event/index.ts
+++ apps/frontend/src/core/event/index.ts
@@ -1,4 +1,6 @@
 import Event from './model/Event';
 import Guest from './model/Guest';
+import createEmptyEvent from './functions/createEmptyEvent';
 
 export type { Event, Guest }
+export { createEmptyEvent }
