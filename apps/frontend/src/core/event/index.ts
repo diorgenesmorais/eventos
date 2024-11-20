aebd87bb439cec49229d4c90d6c1dc561ba6e851
Diorgenes M. Silva <diorgenesmorais@gmail.com>
2024-11-20 18:02:32 -0300

    feat: create completeEvent function

--- apps/frontend/src/core/event/index.ts
+++ apps/frontend/src/core/event/index.ts
@@ -1,6 +1,8 @@
 import Event from './model/Event';
 import Guest from './model/Guest';
 import validateEvent from './functions/validateEvent';
+import completeEvent from './functions/completeEvent';
+import createEmptyEvent from './functions/createEmptyEvent';
 
 export type { Event, Guest }
-export { validateEvent }
+export { validateEvent, completeEvent, createEmptyEvent }
