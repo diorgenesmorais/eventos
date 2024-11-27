6d794831cff4987ac0b0813bb98f42ba3f8c9288
Diorgenes M. Silva <diorgenesmorais@gmail.com>
2024-11-26 22:30:51 -0300

    feat: add btn style in link

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,10 +1,14 @@
 import Page from "@/components/template/Page";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <Page>
-      <div className="">
-        Inicio
+      <div className="flex flex-col">
+        <div className="">
+          Inicio
+        </div>
+        <Link href="/events" className="btn blue w-24" >Eventos</Link>
       </div>
     </Page>
   );
